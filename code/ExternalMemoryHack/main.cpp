b943f593584b147182be87a29eeed503a5646718
hxx.dev <hxx.dev@gmail.com>
2025-01-30 06:34:17 +0900

    removed cached

--- code/ExternalMemoryHack/main.cpp
+++ code/ExternalMemoryHack/main.cpp
@@ -1,19 +1,57 @@
 #ifndef UNICODE
 #define UNICODE
 #endif
+
 #include <Windows.h>
 #include <iostream>
 #include <psapi.h>
+#include <fcntl.h>
+#include <io.h>
 #include <string>
 
 using namespace std;
 
-void AssertError(string ErrorMessage) {
-	cout << "[E] " + ErrorMessage << endl;
+void AssertError(LPCWSTR ErrorMessage) {
+	int msgboxID = MessageBox(
+		NULL,
+		ErrorMessage,
+		(LPCWSTR)L"Error",
+		MB_ICONWARNING | MB_OK | MB_DEFBUTTON1
+	);
+	switch (msgboxID) {
+	case IDOK:
+		// TODO: add code
+		break;
+	}
+	return;
 }
 
-void AssertInfo(string InfoMessage) {
-	cout << "[I] " + InfoMessage << endl;
+void HandleError() {
+	DWORD ErrorCode = GetLastError();
+	LPWSTR message = nullptr;
+
+	if (ErrorCode == 0) {
+		wprintf(L"No error to handle.\n");
+		return;
+	}
+
+	if (!FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM |
+					   FORMAT_MESSAGE_ALLOCATE_BUFFER,
+					   nullptr,
+					   ErrorCode,
+					   MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+					   (LPWSTR) &message,
+					   0,
+					   nullptr)) {
+		wprintf(L"Format message failed with code 0x%x\n", GetLastError());
+		return;
+	}
+
+	if (message) {
+		wcout << message << endl;
+		LocalFree(message);
+	}
+	return;
 }
 
 HMODULE getModule(HANDLE processHandle) {
@@ -21,77 +59,99 @@ HMODULE getModule(HANDLE processHandle) {
 	DWORD cbNeeded;
 	unsigned int i;
 
-	if (EnumProcessModules(processHandle, hMods, sizeof(hMods), &cbNeeded))
+	if (EnumProcessModulesEx(processHandle, hMods, sizeof(hMods), &cbNeeded, LIST_MODULES_64BIT))
 	{
-		cout << "cb" << endl;
-		cout << cbNeeded << endl;
 		for (i = 0; i < (cbNeeded / sizeof(HMODULE)); i++)
 		{
 			TCHAR szModName[MAX_PATH];
 			if (GetModuleFileNameEx(processHandle, hMods[i], szModName, sizeof(szModName) / sizeof(TCHAR)))
 			{
 				wstring wstrModName = szModName;
-				cout << "hi" << endl;
-				wcout << szModName << endl;
 				//you will need to change this to the name of the exe of the foreign process
 				wstring wstrModContain = L"flare.exe";
 				if (wstrModName.find(wstrModContain) != string::npos)
 				{
-					cout << "found" << endl;
-					CloseHandle(processHandle);
 					return hMods[i];
 				}
 			}
 			else {
-				DWORD ErrorCode = GetLastError();
-				cout << ErrorCode << endl;
+				HandleError();
+				return nullptr;
 			}
 		}
+		// no module found
+		wcout << L"no moudle found" << endl;
 		CloseHandle(processHandle);
 	}
 	else {
-		DWORD ErrorCode = GetLastError();
-		LPWSTR message = NULL;
-		ErrorCode = 0x5;
-		//AssertError("EnumProcessModules Error Code :" + to_string(ErrorCode));
-		FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER, nullptr, ErrorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (TCHAR*)&message, 0, nullptr);
-		printf("%s\n", message);
+		HandleError();
+		return nullptr;
 	}
 	return nullptr;
 }
 
 int main(int argc, char** argv) {
-	string ErrorMessage;
-
-	// prefix L casts char* to const wchar_t*.
+	/* get window handle of Flare.exe. */
 	HWND windowHandle = FindWindow(NULL, L"Flare");
 	if (windowHandle == NULL) {
-		AssertError("Window Flare not found");
-		return 0;
+		HandleError();
+		return -1;
 	}
+	wcout << "window found" << endl;
 
+	/* get PID of Flare.exe. */
 	DWORD pid = 0;
-	GetWindowThreadProcessId(windowHandle, &pid);
-	AssertInfo("PID :" + to_string(pid));
+	if (!GetWindowThreadProcessId(windowHandle, &pid)) {
+		HandleError();
+		return -2;
+	}
+	wcout << L"PID :" + to_wstring(pid) << endl;
 
-	HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, true, pid);
-	HMODULE module = getModule(processHandle);
-	DWORD baseAddress = (DWORD)module;
+	/* get process handle of Flare.exe. */
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, true, pid);
+	if (hProcess == NULL) {
+		HandleError();
+		return -3;
+	}
+	/* get base address of process Flare.exe. */
+	HMODULE module = getModule(hProcess);
+	DWORD64 BaseAddress = (DWORD64)module;
+	wcout << hex << L"Base Address :" + to_wstring(BaseAddress) << endl;
+	
+	/* Adjust player position. */
+	DWORD64 PlayerPosxAddress;
+	float PlayerPosx;
+	DWORD64 PlayerPosyAddress;
+	float PlayerPosy;
 
-	AssertInfo("Base Address :" + to_string(baseAddress));
-	/*
-	DWORD gold_address = 0;
-	DWORD gold_value = 0;
 	SIZE_T bytes_read = 0;
-	ReadProcessMemory(process, (LPCVOID)(0x017EECB8 + 0x60), &gold_address, 4, &bytes_read);
-	gold_address += 0xA90;
-	ReadProcessMemory(process, (LPCVOID)gold_address, &gold_address, 4, &bytes_read);
-	gold_address += 0x4;
-	ReadProcessMemory(process, (LPCVOID)gold_address, &gold_value, 4, &bytes_read);
 
-	DWORD new_gold_value = 9999;
-	SIZE_T bytes_written = 0;
-	WriteProcessMemory(process, (LPVOID)gold_address, &new_gold_value, 4, &bytes_written);
-	*/
+	ReadProcessMemory(hProcess, (LPCVOID)(BaseAddress + 0x00189750), &PlayerPosxAddress, sizeof(PlayerPosxAddress), &bytes_read);
+	PlayerPosyAddress = PlayerPosxAddress;
+	PlayerPosxAddress = PlayerPosxAddress + 0x3D0;
+	ReadProcessMemory(hProcess, (LPCVOID)(PlayerPosxAddress), &PlayerPosx, sizeof(PlayerPosx), &bytes_read);
+	PlayerPosyAddress = PlayerPosyAddress + 0x3CC;
+	ReadProcessMemory(hProcess, (LPCVOID)(PlayerPosyAddress), &PlayerPosy, sizeof(PlayerPosy), &bytes_read);
+	wcout << hex << to_wstring(PlayerPosx) << endl;
+	wcout << hex << to_wstring(PlayerPosy) << endl;
+	/*
+	if (~ReadProcessMemory(hProcess, (LPCVOID)(0x22095EB3670), &PlayerPosxAddress, sizeof(PlayerPosxAddress), &bytes_read)) {
+		HandleError();
+		wcout << to_wstring(bytes_read) << endl;
+		PlayerPosx = *(float*)&PlayerPosxAddress;
+		wcout << hex << to_wstring(PlayerPosx) << endl;
+		
+		return -4;
+	}*/
+
+	//printf("bytes read : %d\n", bytes_read);
+	//printf("Player posx Address : %ud\n", PlayerPosxAddress);
+	//ReadProcessMemory(ProcessHandle, (LPCVOID)(PlayerPosxAddress + 0x3D0), & PlayerPosxAddress, 4, & bytes_read);
+	//ReadProcessMemory(ProcessHandle, (LPCVOID)(PlayerPosxAddress), &PlayerPosx, 4, &bytes_read);
+	//printf("Player posx : %f\n", PlayerPosx);
+
+	//SIZE_T bytes_written = 0;
+	//WriteProcessMemory(process, (LPVOID)gold_address, &new_gold_value, 4, &bytes_written);
+	
 	return 0;
 }
